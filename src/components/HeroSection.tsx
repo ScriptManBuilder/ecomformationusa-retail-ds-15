02029f250bb8a69459f75fb85a8c2c73b759a5b2
ScriptManBuilder <dan.gora2004@gmail.com>
2025-12-08 14:47:12 +0200

    Fix issues, update design, fix errors

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -39,11 +39,11 @@ const HeroSection: React.FC = () => {
         </HeroDescription>
         <Tagline>We specialize in serving merchants who acquire customers through <strong style={{color: '#34d399'}}>retail stores</strong>. Our decline recovery technology is optimized for businesses using in-store point-of-sale systems, retail checkout terminals, and brick-and-mortar transaction flows.</Tagline>
         <CTAButtonGroup>
-          <CTAButtonPrimary onClick={handleRequestDemo}>
+          {/* <CTAButtonPrimary onClick={handleRequestDemo}>
             Request a Demo
-          </CTAButtonPrimary>
+          </CTAButtonPrimary> */}
           <CTAButtonSecondary onClick={handleSpeakToSales}>
-            Speak to Sales
+            Schedule a Call
           </CTAButtonSecondary>
         </CTAButtonGroup>
         <Highlight>We serve only retail clients.</Highlight>
